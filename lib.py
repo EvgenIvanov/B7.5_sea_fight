966ea712e1628b5d3f9b5a0a934bc77db7651609
EvgenIvanov <evgen2007@bk.ru>
2020-12-12 18:53:52 +0300

    add comments

--- lib.py
+++ lib.py
@@ -1,10 +1,11 @@
 class Dot:
+    ## фактическая точка игрового поля 
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
-    def __str__(self):
-        return self.__x, self.__y
+    # def __str__(self):
+    #     return self.__x, self.__y
 
 class Ship:
     def __init__(self, Dot, rang = 1, orient = 'h'):
@@ -20,12 +21,14 @@ class Ship:
 
 class Board():
     def __init__(self, size):
-        self.size = size    # размерность игрового поля
+        self.size = size                                                                # размерность игрового поля
         self.deck = [[u'\u2022' for y in range(size)] for x in range(size)]             # точки на доске
         self.dots = set([(x, y) for y in range(self.size) for x in range(self.size)])   # точки плоскости доски для определения возможности парковки корабля
 
     @staticmethod
     def emptyDots(Dot):
+        ## набор точек, окружающих переданную точку
+        #  для вычитания из self.dots точек корабля и точек контура корабля
         x, y = Dot[0], Dot[1]
         return [
             (x-1, y-1), (x, y-1), (x+1, y-1),
@@ -34,11 +37,13 @@ class Board():
         ]
 
     def __add(self, Dot, status):
+        ## добавление точки корабля на доску 
+        # и вычитание из списка self.dots точки и её контура
         if self.deck[Dot[1]][Dot[0]] == u'\u2022':
             self.deck[Dot[1]][Dot[0]] = status
             tDots = self.emptyDots(Dot)
             self.dots.difference_update(tDots)
-            print('self.dots='+str(len(self.dots)))
+            # print('self.dots='+str(len(self.dots)))
 
     def addShip(self, Ship):
         ret = False
